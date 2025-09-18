0b73f05f2648f229866d48bebbe5b79dcb45ac1b
alecus1983 <34242304+alecus1983@users.noreply.github.com>
2025-09-18 07:39:47 -0500

    nodos logicos

---
+++ auto/bibliografia.el
@@ -0,0 +1,28 @@
+(TeX-add-style-hook
+ "bibliografia"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "CREG0701998"
+    "XM-2018-1"
+    "LEY143"
+    "CREG1332013"
+    "EMCALI2007"
+    "NTC20502020"
+    "CREG0251995"
+    "CREG0152018"
+    "NEC2005"
+    "DIGSILENT15"
+    "POWERSADAS2012"
+    "CARLOSF"
+    "RETIE2013"
+    "CREG0942012"
+    "CREG0652000"
+    "CREG2242016"
+    "MANOPEMCALI"
+    "trashorras2015subestaciones"
+    "CREG0611996"
+    "CREG-060-2019"
+    "CREG0282020"
+    "salazar2017"))
+ '(or :bibtex :latex))
+
