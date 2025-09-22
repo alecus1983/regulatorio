714cd809f7e61fc52049082e0bfb192a1d150ce3
alecus1983 <34242304+alecus1983@users.noreply.github.com>
2025-09-21 23:43:48 -0500

    cambios

--- auto/regulatorio.el
+++ auto/regulatorio.el
@@ -5,12 +5,12 @@
                      '(("book" "a5paper")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("geometry" "paperwidth=15cm" "paperheight=20cm") ("inputenc" "utf8") ("fontenc" "T1") ("babel" "spanish" "es-tabla") ("biblatex" "backend=biber" "style=verbose" "hyperref=true") ("xcolor" "table" "dvipsnames") ("caption" "font=small" "labelfont=bf" "labelsep=endash")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -93,6 +93,7 @@
     "fig:capa7iccp"
     "fig:iec61850"
     "fig:iec61850_ln"
+    "fig:ln_example"
     "fig:iec61850resticcion"
     "fig:protocolosiec61850"
     "fig:centralgen"
